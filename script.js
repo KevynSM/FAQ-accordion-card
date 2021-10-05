70b87bbe48bdaaf10a76d1a1ad355b97291af0d8
Kevyn Marins <kevynmarins@gmail.com>
2021-10-05 16:57:39 +0100

    added aria attributes

--- script.js
+++ script.js
@@ -4,12 +4,21 @@ btnList.forEach( (btn) => {
     btn.addEventListener("click", function () {
         this.classList.toggle("isOpen");
 
+        if(this.getAttribute("aria-expanded") === "true") {
+            this.setAttribute("aria-expanded", "false")
+        }
+        else {
+            this.setAttribute("aria-expanded", "true")
+        }
+
         let content = this.nextElementSibling;        
         if(content.style.height !== "") {
             content.style.height = "";
+            content.style.visibility = "hidden";
         }
         else {
             content.style.height = content.scrollHeight + 'px';
+            content.style.visibility = "visible";
         }
         hideAll(this);
     })
@@ -21,6 +30,7 @@ const hideAll = (notThis) => {
             btn.classList.remove("isOpen");
             let content = btn.nextElementSibling;
             content.style.height = "";
+            content.style.visibility = "hidden";
         }
     })
 }
